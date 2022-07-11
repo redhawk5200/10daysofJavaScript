14c728e3bd2598bb5d1d5bdabe6cd98306eded83
Abdul Wahab Khan <abdulwahab052001@gmail.com>
2022-07-11 17:23:52 +0500

    3

--- Day0(Functions).js
+++ Day0(Functions).js
@@ -24,12 +24,46 @@ function readLine() {
 /*
  * Create the function factorial here
  */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
 function factorial(n){
-    let fact=0;
-    for(let i=0;i<n;i++){
-        fact=fact+(n*(n-i-1));
+    //base case
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return n * factorial(n-1);
     }
-    return fact;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
 }
 
 
